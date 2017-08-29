143cf92bc86e55fe7495cafb8fae2dd13d7ea895
faris bueller <dfarisphd@gmail.com>
2017-08-29 09:04:25 -0700

    FINAL Commit - Draft (Needs More Testing)

--- lib/gtest-release-1.8.0/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bueller/Dropbox/DFPHD-800-SWDEV-Google-Protobuf3/kPartialSums/lib/gtest-release-1.8.0
-# Build directory: /Users/bueller/Dropbox/DFPHD-800-SWDEV-Google-Protobuf3/kPartialSums/lib/gtest-release-1.8.0/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(googlemock)
--- lib/gtest-release-1.8.0/build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bueller/Dropbox/DFPHD-800-SWDEV-Google-Protobuf3/kPartialSums/lib/gtest-release-1.8.0/googlemock
-# Build directory: /Users/bueller/Dropbox/DFPHD-800-SWDEV-Google-Protobuf3/kPartialSums/lib/gtest-release-1.8.0/build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
